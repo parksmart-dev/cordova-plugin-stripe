2d168a31823dcd0ed7d0ec5b5f291d3cc82df6c0
Mark Dunn <contactdunny@gmail.com>
2020-10-14 18:32:38 +0100

    Fix

--- src/android/CordovaStripe.java
+++ src/android/CordovaStripe.java
@@ -148,7 +148,15 @@ public class CordovaStripe extends CordovaPlugin
 
     private JSONObject createPaymentDataRequest(String totalPrice, String currencyCode) 
     {
-        final JSONObject tokenizationSpec = new GooglePayConfig().getTokenizationSpecification();
+        //final JSONObject tokenizationSpec = new GooglePayConfig().getTokenizationSpecification();
+
+        final JSONObject tokenizationSpec = new JSONObject()
+            .put("type", WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY)
+            .put(
+                "parameters",
+                new JSONObject()
+                    .put("gateway", "stripe")
+            );
 
         final JSONObject cardPaymentMethod = new JSONObject()
             .put("type", "CARD")
