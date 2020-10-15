76b6192aa3b56a54430fe5b1e4d5b4d2bf2e70d5
Filip <filip@parksmartsolutions.co.uk>
2020-10-15 12:05:53 +0200

    json issue

--- src/android/CordovaStripe.java
+++ src/android/CordovaStripe.java
@@ -234,8 +234,7 @@ public class CordovaStripe extends CordovaPlugin
             )
             .put("merchantInfo", new JSONObject()
                 .put("merchantName", "Example Merchant"))
-            .put("emailRequired", false)
-            .toString();
+            .put("emailRequired", false);
 
         return PaymentDataRequest.fromJson(paymentDataRequest);
     }
