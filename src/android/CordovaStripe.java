aedb7c30f2b61800aac5e918063ea201c38eef5a
Filip <filip@parksmartsolutions.co.uk>
2020-10-16 15:02:36 +0200

    debug

--- src/android/CordovaStripe.java
+++ src/android/CordovaStripe.java
@@ -148,7 +148,7 @@ public class CordovaStripe extends CordovaPlugin
     }
 
 
-    private PaymentDataRequest createPaymentDataRequest(String totalPrice, String currencyCode) 
+    private Optional<JSONObject> createPaymentDataRequest(String totalPrice, String currencyCode) 
     {
 
         return PaymentDataRequest.fromJson("{"
