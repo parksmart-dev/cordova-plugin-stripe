b2e75c6a9db70dede22a6bb22a436750a266c77b
Filip <filip@parksmartsolutions.co.uk>
2020-10-19 14:09:17 +0200

    debug

--- src/android/CordovaStripe.java
+++ src/android/CordovaStripe.java
@@ -296,6 +296,7 @@ public class CordovaStripe extends CordovaPlugin
 
         if (paymentData == null) 
         {
+            googlePayCallbackContext.error("Error with paymentData");
             return;
         }
 
