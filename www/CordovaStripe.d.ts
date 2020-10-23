b88ba7b60be99e2e8cb5632e99a63ef2f2e8e964
Filip <filip@parksmartsolutions.co.uk>
2020-10-23 11:23:04 +0200

    debug

--- www/CordovaStripe.d.ts
+++ www/CordovaStripe.d.ts
@@ -154,12 +154,8 @@ export declare namespace CordovaStripe {
     interface Error {
         message: string;
     }
-    interface PaymentResult {
-        id: string;
-    }
     type BlankCallback = () => void;
     type ErrorCallback = (error: Error) => void;
-    type PaymentSuccessCallback = (result: PaymentResult) => void;
     
     class Plugin {
         /**
@@ -177,6 +173,6 @@ export declare namespace CordovaStripe {
          */
         static payWithApplePay(options: ApplePayOptions, success: (token: TokenResponse, callback: (paymentProcessed: boolean) => void) => void, error?: ErrorCallback): void;
         static initGooglePay(options: GooglePayInit, success?: any, error?: ErrorCallback): void;
-        static payWithGoogle(options: GooglePayOptions, success?: PaymentSuccessCallback, error?: ErrorCallback): void;
+        static payWithGoogle(options: GooglePayOptions, success: (result: string) => void, error?: ErrorCallback): void;
     }
 }
